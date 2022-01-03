8acfcbdb819d5748cf2d7dbdcfd8f942510a183c
Gianelle Tianco <gianelle.tianco@gmail.com>
2022-01-02 20:05:21 -0500

    Changed User Model

--- db/migrate/20220103001907_create_users.rb
+++ db/migrate/20220103001907_create_users.rb
@@ -1,10 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :story
-      t.string :thingsToDo
-      t.string :location
-
+      t.string :eventAnnouncement
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,7 @@ ActiveRecord::Schema.define(version: 2022_01_03_001907) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "story"
-    t.string "thingsToDo"
-    t.string "location"
+    t.string "eventAnnouncement"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
